8a161556da539323672383ddc60298047c611e02
Darren Clark <darren@cwebd.co.uk>
2017-11-15 15:45:47 +0000

    Parse functionality for .ac.uk domains
    Add parsing for .ac.uk with tests for jisc.ac.uk, go files formatted.

--- whois-parser_test.go
+++ whois-parser_test.go
@@ -9,152 +9,152 @@
 
 package whois_parser
 
-
 import (
-    "fmt"
-    "strings"
-    "io/ioutil"
-    "testing"
-    "github.com/bmizerany/assert"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/bmizerany/assert"
 )
 
-
 func TestWhoisParser(t *testing.T) {
-    _, err := Parser("not found")
-    assert.NotEqual(t, err, nil)
-    assert.Equal(t, fmt.Sprintf("%s", err), "Domain is not found.")
+	_, err := Parser("not found")
+	assert.NotEqual(t, err, nil)
+	assert.Equal(t, fmt.Sprintf("%s", err), "Domain is not found.")
 
-    _, err = Parser("WHOIS LIMIT EXCEEDED - SEE WWW.PIR.ORG/WHOIS FOR DETAILS")
-    assert.NotEqual(t, err, nil)
-    assert.Equal(t, fmt.Sprintf("%s", err), "Domain whois data invalid.")
+	_, err = Parser("WHOIS LIMIT EXCEEDED - SEE WWW.PIR.ORG/WHOIS FOR DETAILS")
+	assert.NotEqual(t, err, nil)
+	assert.Equal(t, fmt.Sprintf("%s", err), "Domain whois data invalid.")
 
-    dirs, err := ioutil.ReadDir("./examples/")
-    assert.Equal(t, err, nil)
+	dirs, err := ioutil.ReadDir("./examples/")
+	assert.Equal(t, err, nil)
 
-    for _, v := range dirs {
-        domain := v.Name()
-        domain_ext := domain[strings.LastIndex(domain, ".") + 1:]
-        whois_raw, err := ReadFile("./examples/" + domain)
-        assert.Equal(t, err, nil)
+	for _, v := range dirs {
+		domain := v.Name()
+		domain_ext := domain[strings.LastIndex(domain, ".")+1:]
+		whois_raw, err := ReadFile("./examples/" + domain)
+		assert.Equal(t, err, nil)
 
-        if domain[len(domain) - 4:] == ".out" {
-            continue
-        }
+		if domain[len(domain)-4:] == ".out" {
+			continue
+		}
 
-        whois_info, err := Parser(whois_raw)
-        assert.Equal(t, err, nil)
+		whois_info, err := Parser(whois_raw)
+		assert.Equal(t, err, nil)
 
-        if domain == "mjj.com" {
-            assert.NotEqual(t, whois_info.Registrar.RegistrarID, "")
-        }
-        if domain_ext != "museum" && domain_ext != "at" && domain_ext != "int" {
-            assert.NotEqual(t, whois_info.Registrar.RegistrarName, "")
-        }
-        if domain_ext == "com" {
-            assert.NotEqual(t, whois_info.Registrar.WhoisServer, "")
-        }
-        if domain_ext == "com" {
-            assert.NotEqual(t, whois_info.Registrar.ReferralURL, "")
-        }
-        if domain == "mjj.com" {
-            assert.NotEqual(t, whois_info.Registrar.DomainId, "")
-        }
-        assert.NotEqual(t, whois_info.Registrar.DomainName, "")
-        if domain_ext != "at" && domain_ext != "kr" && domain_ext != "int" {
-            assert.NotEqual(t, whois_info.Registrar.DomainStatus, "")
-        }
-        if domain_ext != "au" && domain_ext != "at" && domain_ext != "int" {
-            assert.NotEqual(t, whois_info.Registrar.CreatedDate, "")
-        }
-        if domain_ext != "cn" && domain_ext != "ru" && domain_ext != "su" && domain_ext != "hk" {
-            assert.NotEqual(t, whois_info.Registrar.UpdatedDate, "")
-        }
-        if domain_ext != "au" && domain_ext != "at" && domain_ext != "re" && domain_ext != "fr" && domain_ext != "int" {
-            assert.NotEqual(t, whois_info.Registrar.ExpirationDate, "")
-        }
-        assert.NotEqual(t, whois_info.Registrar.NameServers, "")
-        if domain_ext == "cn" {
-            assert.NotEqual(t, whois_info.Registrar.DomainDNSSEC, "")
-        }
+		if domain == "mjj.com" {
+			assert.NotEqual(t, whois_info.Registrar.RegistrarID, "")
+		}
 
-        content := ""
-        content += fmt.Sprintf("registrar_id: %s\n", whois_info.Registrar.RegistrarID)
-        content += fmt.Sprintf("registrar_name: %s\n", whois_info.Registrar.RegistrarName)
-        content += fmt.Sprintf("whois_server: %s\n", whois_info.Registrar.WhoisServer)
-        content += fmt.Sprintf("referral_url: %s\n", whois_info.Registrar.ReferralURL)
-        content += fmt.Sprintf("domain_id: %s\n", whois_info.Registrar.DomainId)
-        content += fmt.Sprintf("domain_name: %s\n", whois_info.Registrar.DomainName)
-        content += fmt.Sprintf("domain_status: %s\n", whois_info.Registrar.DomainStatus)
-        content += fmt.Sprintf("created_date: %s\n", whois_info.Registrar.CreatedDate)
-        content += fmt.Sprintf("updated_date: %s\n", whois_info.Registrar.UpdatedDate)
-        content += fmt.Sprintf("expiration_date: %s\n", whois_info.Registrar.ExpirationDate)
-        content += fmt.Sprintf("name_servers: %s\n", whois_info.Registrar.NameServers)
-        content += fmt.Sprintf("domain_dnssec: %s\n", whois_info.Registrar.DomainDNSSEC)
+		if domain_ext != "museum" && domain_ext != "at" && domain_ext != "int" && domain != "jisc.ac.uk" {
+			assert.NotEqual(t, whois_info.Registrar.RegistrarName, "")
+		}
+		if domain_ext == "com" {
+			assert.NotEqual(t, whois_info.Registrar.WhoisServer, "")
+		}
+		if domain_ext == "com" {
+			assert.NotEqual(t, whois_info.Registrar.ReferralURL, "")
+		}
+		if domain == "mjj.com" {
+			assert.NotEqual(t, whois_info.Registrar.DomainId, "")
+		}
+		assert.NotEqual(t, whois_info.Registrar.DomainName, "")
+		if domain_ext != "at" && domain_ext != "kr" && domain_ext != "int" && domain != "jisc.ac.uk" {
+			assert.NotEqual(t, whois_info.Registrar.DomainStatus, "")
+		}
+		if domain_ext != "au" && domain_ext != "at" && domain_ext != "int" {
+			assert.NotEqual(t, whois_info.Registrar.CreatedDate, "")
+		}
+		if domain_ext != "cn" && domain_ext != "ru" && domain_ext != "su" && domain_ext != "hk" && domain != "jisc.ac.uk" {
+			assert.NotEqual(t, whois_info.Registrar.UpdatedDate, "")
+		}
+		if domain_ext != "au" && domain_ext != "at" && domain_ext != "re" && domain_ext != "fr" && domain_ext != "int" {
+			assert.NotEqual(t, whois_info.Registrar.ExpirationDate, "")
+		}
+		assert.NotEqual(t, whois_info.Registrar.NameServers, "")
+		if domain_ext == "cn" {
+			assert.NotEqual(t, whois_info.Registrar.DomainDNSSEC, "")
+		}
 
-        content += fmt.Sprintf("\nregistrant\n")
-        content += fmt.Sprintf("id: %s\n", whois_info.Registrant.ID)
-        content += fmt.Sprintf("name: %s\n", whois_info.Registrant.Name)
-        content += fmt.Sprintf("organization: %s\n", whois_info.Registrant.Organization)
-        content += fmt.Sprintf("street: %s\n", whois_info.Registrant.Street)
-        content += fmt.Sprintf("street_ext: %s\n", whois_info.Registrant.StreetExt)
-        content += fmt.Sprintf("city: %s\n", whois_info.Registrant.City)
-        content += fmt.Sprintf("province: %s\n", whois_info.Registrant.Province)
-        content += fmt.Sprintf("postal_code: %s\n", whois_info.Registrant.PostalCode)
-        content += fmt.Sprintf("country: %s\n", whois_info.Registrant.Country)
-        content += fmt.Sprintf("phone: %s\n", whois_info.Registrant.Phone)
-        content += fmt.Sprintf("phone_ext: %s\n", whois_info.Registrant.PhoneExt)
-        content += fmt.Sprintf("fax: %s\n", whois_info.Registrant.Fax)
-        content += fmt.Sprintf("fax_ext: %s\n", whois_info.Registrant.FaxExt)
-        content += fmt.Sprintf("email: %s\n", whois_info.Registrant.Email)
+		content := ""
+		content += fmt.Sprintf("registrar_id: %s\n", whois_info.Registrar.RegistrarID)
+		content += fmt.Sprintf("registrar_name: %s\n", whois_info.Registrar.RegistrarName)
+		content += fmt.Sprintf("whois_server: %s\n", whois_info.Registrar.WhoisServer)
+		content += fmt.Sprintf("referral_url: %s\n", whois_info.Registrar.ReferralURL)
+		content += fmt.Sprintf("domain_id: %s\n", whois_info.Registrar.DomainId)
+		content += fmt.Sprintf("domain_name: %s\n", whois_info.Registrar.DomainName)
+		content += fmt.Sprintf("domain_status: %s\n", whois_info.Registrar.DomainStatus)
+		content += fmt.Sprintf("created_date: %s\n", whois_info.Registrar.CreatedDate)
+		content += fmt.Sprintf("updated_date: %s\n", whois_info.Registrar.UpdatedDate)
+		content += fmt.Sprintf("expiration_date: %s\n", whois_info.Registrar.ExpirationDate)
+		content += fmt.Sprintf("name_servers: %s\n", whois_info.Registrar.NameServers)
+		content += fmt.Sprintf("domain_dnssec: %s\n", whois_info.Registrar.DomainDNSSEC)
 
-        content += fmt.Sprintf("\nadmin\n")
-        content += fmt.Sprintf("id: %s\n", whois_info.Admin.ID)
-        content += fmt.Sprintf("name: %s\n", whois_info.Admin.Name)
-        content += fmt.Sprintf("organization: %s\n", whois_info.Admin.Organization)
-        content += fmt.Sprintf("street: %s\n", whois_info.Admin.Street)
-        content += fmt.Sprintf("street_ext: %s\n", whois_info.Admin.StreetExt)
-        content += fmt.Sprintf("city: %s\n", whois_info.Admin.City)
-        content += fmt.Sprintf("province: %s\n", whois_info.Admin.Province)
-        content += fmt.Sprintf("postal_code: %s\n", whois_info.Admin.PostalCode)
-        content += fmt.Sprintf("country: %s\n", whois_info.Admin.Country)
-        content += fmt.Sprintf("phone: %s\n", whois_info.Admin.Phone)
-        content += fmt.Sprintf("phone_ext: %s\n", whois_info.Admin.PhoneExt)
-        content += fmt.Sprintf("fax: %s\n", whois_info.Admin.Fax)
-        content += fmt.Sprintf("fax_ext: %s\n", whois_info.Admin.FaxExt)
-        content += fmt.Sprintf("email: %s\n", whois_info.Admin.Email)
+		content += fmt.Sprintf("\nregistrant\n")
+		content += fmt.Sprintf("id: %s\n", whois_info.Registrant.ID)
+		content += fmt.Sprintf("name: %s\n", whois_info.Registrant.Name)
+		content += fmt.Sprintf("organization: %s\n", whois_info.Registrant.Organization)
+		content += fmt.Sprintf("street: %s\n", whois_info.Registrant.Street)
+		content += fmt.Sprintf("street_ext: %s\n", whois_info.Registrant.StreetExt)
+		content += fmt.Sprintf("city: %s\n", whois_info.Registrant.City)
+		content += fmt.Sprintf("province: %s\n", whois_info.Registrant.Province)
+		content += fmt.Sprintf("postal_code: %s\n", whois_info.Registrant.PostalCode)
+		content += fmt.Sprintf("country: %s\n", whois_info.Registrant.Country)
+		content += fmt.Sprintf("phone: %s\n", whois_info.Registrant.Phone)
+		content += fmt.Sprintf("phone_ext: %s\n", whois_info.Registrant.PhoneExt)
+		content += fmt.Sprintf("fax: %s\n", whois_info.Registrant.Fax)
+		content += fmt.Sprintf("fax_ext: %s\n", whois_info.Registrant.FaxExt)
+		content += fmt.Sprintf("email: %s\n", whois_info.Registrant.Email)
 
-        content += fmt.Sprintf("\ntech\n")
-        content += fmt.Sprintf("id: %s\n", whois_info.Tech.ID)
-        content += fmt.Sprintf("name: %s\n", whois_info.Tech.Name)
-        content += fmt.Sprintf("organization: %s\n", whois_info.Tech.Organization)
-        content += fmt.Sprintf("street: %s\n", whois_info.Tech.Street)
-        content += fmt.Sprintf("street_ext: %s\n", whois_info.Tech.StreetExt)
-        content += fmt.Sprintf("city: %s\n", whois_info.Tech.City)
-        content += fmt.Sprintf("province: %s\n", whois_info.Tech.Province)
-        content += fmt.Sprintf("postal_code: %s\n", whois_info.Tech.PostalCode)
-        content += fmt.Sprintf("country: %s\n", whois_info.Tech.Country)
-        content += fmt.Sprintf("phone: %s\n", whois_info.Tech.Phone)
-        content += fmt.Sprintf("phone_ext: %s\n", whois_info.Tech.PhoneExt)
-        content += fmt.Sprintf("fax: %s\n", whois_info.Tech.Fax)
-        content += fmt.Sprintf("fax_ext: %s\n", whois_info.Tech.FaxExt)
-        content += fmt.Sprintf("email: %s\n", whois_info.Tech.Email)
+		content += fmt.Sprintf("\nadmin\n")
+		content += fmt.Sprintf("id: %s\n", whois_info.Admin.ID)
+		content += fmt.Sprintf("name: %s\n", whois_info.Admin.Name)
+		content += fmt.Sprintf("organization: %s\n", whois_info.Admin.Organization)
+		content += fmt.Sprintf("street: %s\n", whois_info.Admin.Street)
+		content += fmt.Sprintf("street_ext: %s\n", whois_info.Admin.StreetExt)
+		content += fmt.Sprintf("city: %s\n", whois_info.Admin.City)
+		content += fmt.Sprintf("province: %s\n", whois_info.Admin.Province)
+		content += fmt.Sprintf("postal_code: %s\n", whois_info.Admin.PostalCode)
+		content += fmt.Sprintf("country: %s\n", whois_info.Admin.Country)
+		content += fmt.Sprintf("phone: %s\n", whois_info.Admin.Phone)
+		content += fmt.Sprintf("phone_ext: %s\n", whois_info.Admin.PhoneExt)
+		content += fmt.Sprintf("fax: %s\n", whois_info.Admin.Fax)
+		content += fmt.Sprintf("fax_ext: %s\n", whois_info.Admin.FaxExt)
+		content += fmt.Sprintf("email: %s\n", whois_info.Admin.Email)
 
-        content += fmt.Sprintf("\nbill\n")
-        content += fmt.Sprintf("id: %s\n", whois_info.Bill.ID)
-        content += fmt.Sprintf("name: %s\n", whois_info.Bill.Name)
-        content += fmt.Sprintf("organization: %s\n", whois_info.Bill.Organization)
-        content += fmt.Sprintf("street: %s\n", whois_info.Bill.Street)
-        content += fmt.Sprintf("street_ext: %s\n", whois_info.Bill.StreetExt)
-        content += fmt.Sprintf("city: %s\n", whois_info.Bill.City)
-        content += fmt.Sprintf("province: %s\n", whois_info.Bill.Province)
-        content += fmt.Sprintf("postal_code: %s\n", whois_info.Bill.PostalCode)
-        content += fmt.Sprintf("country: %s\n", whois_info.Bill.Country)
-        content += fmt.Sprintf("phone: %s\n", whois_info.Bill.Phone)
-        content += fmt.Sprintf("phone_ext: %s\n", whois_info.Bill.PhoneExt)
-        content += fmt.Sprintf("fax: %s\n", whois_info.Bill.Fax)
-        content += fmt.Sprintf("fax_ext: %s\n", whois_info.Bill.FaxExt)
-        content += fmt.Sprintf("email: %s\n", whois_info.Bill.Email)
+		content += fmt.Sprintf("\ntech\n")
+		content += fmt.Sprintf("id: %s\n", whois_info.Tech.ID)
+		content += fmt.Sprintf("name: %s\n", whois_info.Tech.Name)
+		content += fmt.Sprintf("organization: %s\n", whois_info.Tech.Organization)
+		content += fmt.Sprintf("street: %s\n", whois_info.Tech.Street)
+		content += fmt.Sprintf("street_ext: %s\n", whois_info.Tech.StreetExt)
+		content += fmt.Sprintf("city: %s\n", whois_info.Tech.City)
+		content += fmt.Sprintf("province: %s\n", whois_info.Tech.Province)
+		content += fmt.Sprintf("postal_code: %s\n", whois_info.Tech.PostalCode)
+		content += fmt.Sprintf("country: %s\n", whois_info.Tech.Country)
+		content += fmt.Sprintf("phone: %s\n", whois_info.Tech.Phone)
+		content += fmt.Sprintf("phone_ext: %s\n", whois_info.Tech.PhoneExt)
+		content += fmt.Sprintf("fax: %s\n", whois_info.Tech.Fax)
+		content += fmt.Sprintf("fax_ext: %s\n", whois_info.Tech.FaxExt)
+		content += fmt.Sprintf("email: %s\n", whois_info.Tech.Email)
 
-        WriteFile("./examples/" + v.Name() + ".out", content)
-    }
+		content += fmt.Sprintf("\nbill\n")
+		content += fmt.Sprintf("id: %s\n", whois_info.Bill.ID)
+		content += fmt.Sprintf("name: %s\n", whois_info.Bill.Name)
+		content += fmt.Sprintf("organization: %s\n", whois_info.Bill.Organization)
+		content += fmt.Sprintf("street: %s\n", whois_info.Bill.Street)
+		content += fmt.Sprintf("street_ext: %s\n", whois_info.Bill.StreetExt)
+		content += fmt.Sprintf("city: %s\n", whois_info.Bill.City)
+		content += fmt.Sprintf("province: %s\n", whois_info.Bill.Province)
+		content += fmt.Sprintf("postal_code: %s\n", whois_info.Bill.PostalCode)
+		content += fmt.Sprintf("country: %s\n", whois_info.Bill.Country)
+		content += fmt.Sprintf("phone: %s\n", whois_info.Bill.Phone)
+		content += fmt.Sprintf("phone_ext: %s\n", whois_info.Bill.PhoneExt)
+		content += fmt.Sprintf("fax: %s\n", whois_info.Bill.Fax)
+		content += fmt.Sprintf("fax_ext: %s\n", whois_info.Bill.FaxExt)
+		content += fmt.Sprintf("email: %s\n", whois_info.Bill.Email)
+
+		WriteFile("./examples/"+v.Name()+".out", content)
+	}
 }
--- whois-utils.go
+++ whois-utils.go
@@ -9,168 +9,164 @@
 
 package whois_parser
 
-
 import (
-    "strings"
-    "io/ioutil"
+	"io/ioutil"
+	"strings"
 )
 
-
 func ReadFile(file string) (result string, err error) {
-    tmp_result, err := ioutil.ReadFile(file)
-    if err != nil {
-        return
-    }
+	tmp_result, err := ioutil.ReadFile(file)
+	if err != nil {
+		return
+	}
 
-    result = string(tmp_result)
-    return
+	result = string(tmp_result)
+	return
 }
-
 
 func WriteFile(file string, data string) (err error) {
-    err = ioutil.WriteFile(file, []byte(data), 0644)
-    return
+	err = ioutil.WriteFile(file, []byte(data), 0644)
+	return
 }
-
 
 func IsNotFound(data string) (result bool) {
-    data = strings.ToLower(data)
-    return strings.Contains(data, "no found") || strings.Contains(data, "no match") ||
-        strings.Contains(data, "not found") || strings.Contains(data, "not match") ||
-        strings.Contains(data, "no entries found") || strings.Contains(data, "no data found") ||
-        strings.Contains(data, "not registered") || strings.Contains(data, "is free")
+	data = strings.ToLower(data)
+	return strings.Contains(data, "no found") || strings.Contains(data, "no match") ||
+		strings.Contains(data, "not found") || strings.Contains(data, "not match") ||
+		strings.Contains(data, "no entries found") || strings.Contains(data, "no data found") ||
+		strings.Contains(data, "not registered") || strings.Contains(data, "is free")
 }
 
+func TransferName(name string) string {
+	name = strings.ToLower(name)
+	name = strings.Replace(name, "-", " ", -1)
+	name = strings.Replace(name, "_", " ", -1)
+	name = strings.Replace(name, "\\", "/", -1)
 
-func TransferName(name string) (string) {
-    name = strings.ToLower(name)
-    name = strings.Replace(name, "-", " ", -1)
-    name = strings.Replace(name, "_", " ", -1)
-    name = strings.Replace(name, "\\", "/", -1)
+	if strings.Contains(name, "(") {
+		names := strings.Split(name, "(")
+		name = names[0]
+	}
 
-    if strings.Contains(name, "(") {
-        names := strings.Split(name, "(")
-        name = names[0]
-    }
+	name = strings.Replace(name, "domain name", "domain", -1)
 
-    name = strings.Replace(name, "domain name", "domain", -1)
+	name = strings.Replace(name, "domain ", "", -1)
+	name = strings.Replace(name, "sponsoring ", "", -1)
+	name = strings.Replace(name, "registry ", "", -1)
+	name = strings.Replace(name, "information", "", -1)
 
-    name = strings.Replace(name, "domain ", "", -1)
-    name = strings.Replace(name, "sponsoring ", "", -1)
-    name = strings.Replace(name, "registry ", "", -1)
-    name = strings.Replace(name, "information", "", -1)
+	name = strings.Replace(name, " contact", "", -1)
+	name = strings.Replace(name, " number", "", -1)
 
-    name = strings.Replace(name, " contact", "", -1)
-    name = strings.Replace(name, " number", "", -1)
+	name = strings.Replace(name, "registrar iana", "registrar", -1)
+	name = strings.Replace(name, "registrar name", "registrar", -1)
+	name = strings.Replace(name, "registrar organization", "registrar", -1)
+	name = strings.Replace(name, "registrar registration", "registrar", -1)
+	name = strings.Replace(name, "registrar whois", "whois", -1)
+	name = strings.Replace(name, "registrar url", "referral url", -1)
+	name = strings.Replace(name, "registrar registration", "registration", -1)
+	name = strings.Replace(name, "authorized agency", "registrar", -1)
 
-    name = strings.Replace(name, "registrar iana", "registrar", -1)
-    name = strings.Replace(name, "registrar name", "registrar", -1)
-    name = strings.Replace(name, "registrar organization", "registrar", -1)
-    name = strings.Replace(name, "registrar registration", "registrar", -1)
-    name = strings.Replace(name, "registrar whois", "whois", -1)
-    name = strings.Replace(name, "registrar url", "referral url", -1)
-    name = strings.Replace(name, "registrar registration", "registration", -1)
-    name = strings.Replace(name, "authorized agency", "registrar", -1)
+	name = strings.Replace(name, "host name", "name server", -1)
+	name = strings.Replace(name, "nserver", "name server", -1)
+	name = strings.Replace(name, "nameservers", "name server", -1)
+	name = strings.Replace(name, "name servers", "name server", -1)
+	name = strings.Replace(name, "dns server", "name server", -1)
+	name = strings.Replace(name, "dns name server", "name server", -1)
+	name = strings.Replace(name, "servers", "name server", -1)
 
-    name = strings.Replace(name, "host name", "name server", -1)
-    name = strings.Replace(name, "nserver", "name server", -1)
-    name = strings.Replace(name, "nameservers", "name server", -1)
-    name = strings.Replace(name, "name servers", "name server", -1)
-    name = strings.Replace(name, "dns server", "name server", -1)
-    name = strings.Replace(name, "dns name server", "name server", -1)
+	name = strings.Replace(name, " on", " date", -1)
+	name = strings.Replace(name, " at", " date", -1)
 
-    name = strings.Replace(name, " on", " date", -1)
-    name = strings.Replace(name, " at", " date", -1)
+	name = strings.Replace(name, "created", "create", -1)
+	name = strings.Replace(name, "creation", "create", -1)
+	name = strings.Replace(name, "registered date", "create date", -1)
+	name = strings.Replace(name, "registration date", "create date", -1)
+	name = strings.Replace(name, "domain created", "create", -1)
+	name = strings.Replace(name, "domain registered", "create", -1)
+	name = strings.Replace(name, "commencement date", "create", -1)
+	name = strings.Replace(name, "create date", "create", -1)
+	name = strings.Replace(name, "entry create", "create", -1)
 
-    name = strings.Replace(name, "created", "create", -1)
-    name = strings.Replace(name, "creation", "create", -1)
-    name = strings.Replace(name, "registered date", "create date", -1)
-    name = strings.Replace(name, "registration date", "create date", -1)
-    name = strings.Replace(name, "domain created", "create", -1)
-    name = strings.Replace(name, "domain registered", "create", -1)
-    name = strings.Replace(name, "commencement date", "create", -1)
-    name = strings.Replace(name, "create date", "create", -1)
+	name = strings.Replace(name, "updated", "update", -1)
+	name = strings.Replace(name, "modified", "update", -1)
+	name = strings.Replace(name, "changed", "update", -1)
+	name = strings.Replace(name, "last update", "update", -1)
+	name = strings.Replace(name, "domain updated", "update", -1)
+	name = strings.Replace(name, "update date", "update", -1)
+	name = strings.Replace(name, "entry update", "update", -1)
 
-    name = strings.Replace(name, "updated", "update", -1)
-    name = strings.Replace(name, "modified", "update", -1)
-    name = strings.Replace(name, "changed", "update", -1)
-    name = strings.Replace(name, "last update", "update", -1)
-    name = strings.Replace(name, "domain updated", "update", -1)
-    name = strings.Replace(name, "update date", "update", -1)
+	name = strings.Replace(name, "expires", "expire", -1)
+	name = strings.Replace(name, "paid till", "expire", -1)
+	name = strings.Replace(name, "expiration", "expire", -1)
+	name = strings.Replace(name, "expiry", "expire", -1)
+	name = strings.Replace(name, "registrar expire", "expire", -1)
+	name = strings.Replace(name, "domain expires", "expire", -1)
+	name = strings.Replace(name, "expire date", "expire", -1)
+	name = strings.Replace(name, "renewal date", "expire", -1)
 
-    name = strings.Replace(name, "expires", "expire", -1)
-    name = strings.Replace(name, "paid till", "expire", -1)
-    name = strings.Replace(name, "expiration", "expire", -1)
-    name = strings.Replace(name, "expiry", "expire", -1)
-    name = strings.Replace(name, "registrar expire", "expire", -1)
-    name = strings.Replace(name, "domain expires", "expire", -1)
-    name = strings.Replace(name, "expire date", "expire", -1)
+	name = strings.Replace(name, "owner", "registrant", -1)
+	name = strings.Replace(name, "administrative", "admin", -1)
+	name = strings.Replace(name, "technical", "tech", -1)
+	name = strings.Replace(name, "billing", "bill", -1)
 
-    name = strings.Replace(name, "owner", "registrant", -1)
-    name = strings.Replace(name, "administrative", "admin", -1)
-    name = strings.Replace(name, "technical", "tech", -1)
-    name = strings.Replace(name, "billing", "bill", -1)
+	name = strings.Replace(name, "address1", "street", -1)
+	name = strings.Replace(name, "address2", "street_ext", -1)
+	name = strings.Replace(name, "street1", "street", -1)
+	name = strings.Replace(name, "street2", "street_ext", -1)
+	name = strings.Replace(name, "state/", "", -1)
+	name = strings.Replace(name, "/economy", "", -1)
 
-    name = strings.Replace(name, "address1", "street", -1)
-    name = strings.Replace(name, "address2", "street_ext", -1)
-    name = strings.Replace(name, "street1", "street", -1)
-    name = strings.Replace(name, "street2", "street_ext", -1)
-    name = strings.Replace(name, "state/", "", -1)
-    name = strings.Replace(name, "/economy", "", -1)
+	name = strings.Replace(name, "state", "status", -1)
+	name = strings.Replace(name, "facsimile", "fax", -1)
 
-    name = strings.Replace(name, "state", "status", -1)
-    name = strings.Replace(name, "facsimile", "fax", -1)
+	name = strings.Replace(name, "registration status", "status", -1)
+	name = strings.Replace(name, "e mail", "email", -1)
 
-    name = strings.Replace(name, "registration status", "status", -1)
-    name = strings.Replace(name, "e mail", "email", -1)
+	name = strings.Replace(name, "  ", " ", -1)
+	name = strings.Trim(name, " ")
 
-    name = strings.Replace(name, "  ", " ", -1)
-    name = strings.Trim(name, " ")
+	if name == "admin c" {
+		name = "admin"
+	}
+	if name == "tech c" {
+		name = "tech"
+	}
+	if name == "bill c" {
+		name = "bill"
+	}
 
-    if name == "admin c" {
-        name = "admin"
-    }
-    if name == "tech c" {
-        name = "tech"
-    }
-    if name == "bill c" {
-        name = "bill"
-    }
-
-    return name
+	return name
 }
-
 
 func RemoveDuplicateField(data string) string {
-    new_fields := []string{}
-    fields := strings.Split(data, ",")
-    for _, v := range fields {
-        if v == "" {
-            continue
-        }
-        if !StringInArray(new_fields, v) {
-            new_fields = append(new_fields, v)
-        }
-    }
-    return strings.Join(new_fields, ",")
+	new_fields := []string{}
+	fields := strings.Split(data, ",")
+	for _, v := range fields {
+		if v == "" {
+			continue
+		}
+		if !StringInArray(new_fields, v) {
+			new_fields = append(new_fields, v)
+		}
+	}
+	return strings.Join(new_fields, ",")
 }
-
 
 func StringInArray(array []string, find string) bool {
-    for _, v := range array {
-        if v == find {
-            return true
-        }
-    }
-    return false
+	for _, v := range array {
+		if v == find {
+			return true
+		}
+	}
+	return false
 }
 
-
 func FixNameServers(nservers string) string {
-    servers := strings.Split(nservers, ",")
-    for k, v := range servers {
-        names := strings.Split(strings.Trim(v, " "), " ")
-        servers[k] = names[0]
-    }
-    return strings.Join(servers, ",")
+	servers := strings.Split(nservers, ",")
+	for k, v := range servers {
+		names := strings.Split(strings.Trim(v, " "), " ")
+		servers[k] = names[0]
+	}
+	return strings.Join(servers, ",")
 }
